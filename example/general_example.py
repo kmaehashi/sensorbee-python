33b33a53a4a308ae6c9779db83ee143f347d416b
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2016-12-24 18:22:01 +0900

    add example of synchronous query API

--- example/general_example.py
+++ example/general_example.py
@@ -16,6 +16,16 @@ class GeneralExample(object):
         print(api.streams('beepy_test'))
         print(api.sinks('beepy_test'))
         print(api.query('beepy_test', 'EVAL 1+2+3;'))
+
+        print(api.query('beepy_test', 'CREATE SOURCE ns TYPE node_statuses;'))
+        rs = api.query('beepy_test', 'SELECT RSTREAM * FROM ns [RANGE 1 TUPLES];')
+        count = 3
+        for r in rs:
+            print(r)
+            count -= 1
+            if count <= 0:
+                break
+
         print(api.delete_topology('beepy_test'))
         print(api.topologies())
 
